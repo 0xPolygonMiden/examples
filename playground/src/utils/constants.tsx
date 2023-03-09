db4e522196c14e7d6075fbc8fc25d30228937a1e
Dominik1999 <schmid.dominik.muc@gmail.com>
2023-03-09 09:20:20 +0100

    refactor: putting constants to constants.tsx

---
+++ playground/src/utils/constants.tsx
@@ -0,0 +1,12 @@
+export const exampleInput = `{
+    "stack_init": ["0"],
+    "advice_tape": ["0"]
+}`
+
+export const exampleCode = `begin
+  push.1
+  push.2
+  add
+end`
+
+export const emptyOutput = "\n \n \n \n"
