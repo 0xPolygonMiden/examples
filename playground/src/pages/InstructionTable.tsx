997834feb8f83dbed52807206bc65e8989a544c0
Dominik1999 <schmid.dominik.muc@gmail.com>
2023-03-17 17:28:20 +0100

    feat: search in instruction and minor tweaks

--- playground/src/pages/InstructionTable.tsx
+++ playground/src/pages/InstructionTable.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import math from "remark-math";
@@ -12,18 +12,33 @@ function classNames(...classes: string[]) {
 }
 
 /** TODO:
- * Search button does not work yet
- * Expand button does not work yet - either remove or implement
- * Decide on a table layout and pagination
  * Table loads slowly - consider loading only the first 10 rows and then loading more as the user scrolls
  * or use react table (https://react-table.tanstack.com/docs/overview)`
  */
 export default function InstructionTable() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <div className="px-4 sm:px-6 lg:px-8 pt-3">
+<div className="px-4 sm:px-6 lg:px-8 pt-6">
+  <div className="flex grid-col-2 justify-between">
+    <h1 className="text-xl font-semibold text-blue">Instructions</h1>
+    <div className="mt-4">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+        placeholder="Search instructions"
+      />
+    </div>
+  </div>
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Instructions</h1>
           <p className="mt-2 text-sm text-gray-700">
             A list of all Miden assembly instructions. If you want to learn more
             about Miden Assembly and the Miden VM you can read the
@@ -39,11 +54,11 @@ export default function InstructionTable() {
         </div>
       </div>
       <div className="mt-8 flex flex-col">
-        <div className="table -my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+        <div className="table -my-2 -mx-4 sm:-mx-6 lg:-mx-8 max-h-96">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <div className="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
               <table className="min-w-full">
-                <thead className="bg-white">
+                <thead className="bg-white sticky top-0 z-10">
                   <tr>
                     <th
                       scope="col"
@@ -77,19 +92,27 @@ export default function InstructionTable() {
                     </th>
                   </tr>
                 </thead>
-                <tbody className="bg-white">
-                  {assemblerInstructions.map((instructionClass) => (
+                <tbody className="bg-white h-96 overflow-y-auto">
+                  {assemblerInstructions
+                    .filter((instructionClass) =>
+                      instructionClass.instructions.some((instruction) =>
+                        instruction.instruction.toLowerCase().includes(searchQuery.toLowerCase())
+                      )
+                    )
+                    .map((instructionClass) => (
                     <Fragment key={instructionClass.class}>
                       <tr className="border-t border-gray-200">
                         <th
                           colSpan={6}
                           scope="colgroup"
-                          className="bg-gray-50 px-4 py-2 text-left text-sm font-semibold text-gray-900 sm:px-6"
+                          className="bg-gray-50 px-4 py-2 text-left text-sm font-semibold text-black sm:px-6"
                         >
                           {instructionClass.class}
                         </th>
                       </tr>
-                      {instructionClass.instructions.map(
+                      {instructionClass.instructions
+                        .filter(instruction => instruction.instruction.toLowerCase().includes(searchQuery.toLowerCase()))
+                        .map(
                         (instruction, instructionIdx) => (
                           <tr
                             key={instruction.instruction}
@@ -100,7 +123,7 @@ export default function InstructionTable() {
                               "border-t"
                             )}
                           >
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-black sm:pl-6">
                               <ReactMarkdown
                                 remarkPlugins={[math]}
                                 rehypePlugins={[katex]}
@@ -108,7 +131,7 @@ export default function InstructionTable() {
                                 {instruction.instruction}
                               </ReactMarkdown>
                             </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-black">
                               <ReactMarkdown
                                 remarkPlugins={[math, gfm]}
                                 rehypePlugins={[katex]}
@@ -116,7 +139,7 @@ export default function InstructionTable() {
                                 {instruction.stackInput}
                               </ReactMarkdown>
                             </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-black">
                               <ReactMarkdown
                                 remarkPlugins={[math, gfm]}
                                 rehypePlugins={[katex]}
@@ -124,7 +147,7 @@ export default function InstructionTable() {
                                 {instruction.stackOutput}
                               </ReactMarkdown>
                             </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-black">
                               <ReactMarkdown
                                 remarkPlugins={[math, gfm]}
                                 rehypePlugins={[katex]}
@@ -132,7 +155,7 @@ export default function InstructionTable() {
                                 {instruction.cycles}
                               </ReactMarkdown>
                             </td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-black">
                               <ReactMarkdown
                                 remarkPlugins={[math, gfm]}
                                 rehypePlugins={[katex]}
