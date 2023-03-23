eca9083a47aa36f42fa8fd720074bd6d540c00bf
grjte <grjte@protonmail.com>
2023-03-23 20:56:55 +0000

    feat: new responsive header design

--- playground/src/components/NavBar.tsx
+++ playground/src/components/NavBar.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import Link from "../components/Link";
 import MidenLogo from "./MidenLogo";
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+
+const navigation = [
+  { name: 'Documentation', href: 'https://wiki.polygon.technology/docs/miden/user_docs/assembly/main' },
+  { name: 'Examples', href: 'https://github.com/0xPolygonMiden/examples#available-examples' },
+  { name: 'Homepage', href: 'https://polygon.technology/solutions/polygon-miden/' },
+]
 
 type TabType = {
   name: string;
@@ -21,6 +29,7 @@ function classNames(...classes: any[]) {
 
 const NavBar = () => {
 
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState('Playground');
 
@@ -39,33 +48,70 @@ const NavBar = () => {
 
   return (
     <>
-      <div className="flex items-center justify-between flex-wrap bg-blue-700">
-        <div
-          className="flex items-center flex-shrink-0 text-white mr-6"
-          data-testid="logo"
-        >
-          <MidenLogo />
-          <span className="font-semibold text-xl tracking-tight">
-            MidenVM Playground
-          </span>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow" data-testid="top-links">
-            <Link
-              label="Documentation"
-              address="https://wiki.polygon.technology/docs/miden/user_docs/assembly/main"
-            />
-            <Link
-              label="Examples"
-              address="https://github.com/0xPolygonMiden/examples#available-examples"
-            />
-            <Link
-              label="Homepage"
-              address="https://polygon.technology/solutions/polygon-miden/"
-            />
+      <header className="bg-gray-900">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex items-center">
+            <a href="https://polygon.technology/solutions/polygon-miden/" className="-m-1.5 px-1.5">
+              <span className="sr-only">Polygon Miden</span>
+              <MidenLogo className="fill-white h-10 w-auto" />
+            </a>
+            <h1 className="text-xl font-semibold leading-6 text-white">MidenVM Playground</h1>
           </div>
-        </div>
-      </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} target="_blank" rel="noopener noreferrer" className="text-sm font-semibold leading-6 text-white">
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-10" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Polygon Miden</span>
+                <MidenLogo className="fill-gray-900 h-10 w-auto" />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
       <div>
         <div className="sm:hidden">
           <label htmlFor="tabs" className="sr-only">
