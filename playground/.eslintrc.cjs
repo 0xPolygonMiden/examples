858326a8c8af3569846f7cd29498befb7aaa8c06
gubloon <7201911+gubloon@users.noreply.github.com>
2024-04-25 17:43:41 +0530

    Update .eslintrc.cjs

--- playground/.eslintrc.cjs
+++ playground/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true
