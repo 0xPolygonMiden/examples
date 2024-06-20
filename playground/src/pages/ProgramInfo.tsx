843078574c6c2972fdc5db8d922a9885f353bcf1
darkwolve <kaushal@oakrev.com>
2024-06-20 07:13:48 +0530

    design fixes for program info

--- playground/src/pages/ProgramInfo.tsx
+++ playground/src/pages/ProgramInfo.tsx
@@ -6,12 +6,10 @@ type ProgramInfoProps = {
 
 const ProgramInfo = (props: ProgramInfoProps): JSX.Element => {
   return (
-    <div className="flex w-full h-fit rounded-xl border overflow-y-scroll border-secondary-4">
+    <div className="flex w-full h-fit rounded-xl border overflow-y-scroll border-borderColor">
       <div className="flex flex-col w-full">
-        <div className="bg-secondary-main z-10 py-4 flex sticky top-0 text-white items-center">
-          <h1 className="pl-5 text-left text-base font-semibold">
-            Program Info
-          </h1>
+        <div className="bg-secondary-main z-10 py-4 flex sticky top-0 text-secondary-7 items-center">
+          <h1 className="pl-5 text-left text-base font-normal">Program Info</h1>
         </div>
 
         <div className="h-px bg-secondary-4"></div>
